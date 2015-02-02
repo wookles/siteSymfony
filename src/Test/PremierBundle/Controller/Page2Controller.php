04225b3b0226dc06fc384a5dc0f315e92d5f1696
Marechal Charlie <spi3029@ic2s130-02.univ-lemans.fr>
2015-02-02 12:13:06 +0100

    Ajout de src

---
+++ src/Test/PremierBundle/Controller/Page2Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+// src/OC/PlatformBundle/Controller/PAge2Controller.php
+
+namespace Test\PremierBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class Page2Controller
+{
+    public function indexAction()
+    {
+        return new Response("byby memberzZz");
+    }
+}
---
+++ src/Test/PremierBundle/TestPremierBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Test\PremierBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TestPremierBundle extends Bundle
+{
+}
