e48805545c54a4f9cf9ca290d347f32f5cd60361
Lomundil <cmarechal53@hotmail.fr>
2015-02-27 18:26:19 +0100

    Modif

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new ActorzzzBundle\ActorzzzBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
