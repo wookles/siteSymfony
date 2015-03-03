33fdbaa739f58148feef73409462755621b798ae
Lomundil <cmarechal53@hotmail.fr>
2015-03-03 10:32:47 +0100

    Modif dans le ContactController

--- src/ActorzzzBundle/Controller/ContactzzzController.php
+++ src/ActorzzzBundle/Controller/ContactzzzController.php
@@ -1,9 +1,12 @@
 <?php
 
+
 namespace ActorzzzBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use ActorzzzBundle\Entity\Formulaire;
 
 class ContactzzzController extends Controller
 {
@@ -12,4 +15,24 @@ class ContactzzzController extends Controller
        return $this->render('ActorzzzBundle:ContactView:Contact.html.twig');
 
     }
+    
+    public function newAction(Request $request){
+    
+    	$formContact = new Formulaire();
+    	$formContact->setEmail('email@test.fr');
+    	$formContact->setNom('Entrez votre nom');
+    	$formContact->setPrenom('Entrez votre prénom');
+    	$formContact->setLienImage('lien de votre image');
+    	
+    	$form = $this->createFormBuilder($formContact)
+    		->add('email', 'email')
+    		->add('nom', 'text')
+    		->add('prenom', 'text')
+    		->add('lienImage', 'text')
+    		->add('save', 'submit')
+    		->getForm();
+    		
+    	return $this->render('ActorzzzBundle:ContactView:Contact.html.twig', array('form' => $form->createView(),));
+	}
 }
+?>
