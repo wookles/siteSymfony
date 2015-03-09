cb54a9c8ceaefa5c37787be0bf48fc8e2fa36c16
wookles <p.armanger@laposte.net>
2015-03-09 10:06:37 +0100

    création des 3 premières classes ds src/Actorzzz/Entity

---
+++ src/ActorzzzBundle/Entity/lien.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace ActorzzzBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * lien
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="ActorzzzBundle\Entity\lienRepository")
+ */
+class lien
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="type_lien", type="integer")
+     */
+    private $typeLien;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom_lien", type="string", length=512)
+     */
+    private $nomLien;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set typeLien
+     *
+     * @param integer $typeLien
+     * @return lien
+     */
+    public function setTypeLien($typeLien)
+    {
+        $this->typeLien = $typeLien;
+
+        return $this;
+    }
+
+    /**
+     * Get typeLien
+     *
+     * @return integer 
+     */
+    public function getTypeLien()
+    {
+        return $this->typeLien;
+    }
+
+    /**
+     * Set nomLien
+     *
+     * @param string $nomLien
+     * @return lien
+     */
+    public function setNomLien($nomLien)
+    {
+        $this->nomLien = $nomLien;
+
+        return $this;
+    }
+
+    /**
+     * Get nomLien
+     *
+     * @return string 
+     */
+    public function getNomLien()
+    {
+        return $this->nomLien;
+    }
+}
