9bbdf2d4cd500d72e03a3bf8527d9ace195032a9
Lomundil <cmarechal53@hotmail.fr>
2015-03-11 14:39:41 +0100

    Ajout du controller pour les inscriptions

---
+++ src/ActorzzzBundle/Controller/InscriptionController.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace ActorzzzBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use ActorzzzBundle\Entity\formulaireInscription;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class InscriptionController extends Controller
+{
+    public function indexAction()
+    {
+       return $this->render('ActorzzzBundle:InscriptionView:Inscription.html.twig');
+
+    }
+    
+    public function newAction(Request $request){
+    
+    	$formInscrip = new FormulaireInscription();
+    	$formInscrip->setEmail('email@test.fr');
+    	$formInscrip->setNom('Entrez votre nom');
+    	$formInscrip->setPrenom('Entrez votre prénom');
+    	$formInscrip->setPseudo('Entrez votre pseudo');
+    	$formInscrip->setPass('Entrez votre mot de passe');
+    	$formInscrip->setLienImage('lien de votre image');
+    	
+    	$form = $this->createFormBuilder($formContact)
+    		->add('email', 'email')
+    		->add('nom', 'text')
+    		->add('prenom', 'text')
+    		->add('pseudo', 'text')
+    		->add('motdepasse', 'text')
+    		->add('lienImage', 'text')
+    		->add('save', 'submit')
+    		->getForm();
+    		
+    	return $this->render('ActorzzzBundle:InscriptionView:inscription.html.twig', array('form' => $form->createView(),));
+	}
+}
